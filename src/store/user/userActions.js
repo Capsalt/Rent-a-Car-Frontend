21b811ad9973c7103298eea7afd0543edca775a2
Capsalt <hbasri2304@gmail.com>
2021-12-06 21:40:20 +0300

    v6

---
+++ src/store/user/userActions.js
@@ -0,0 +1,17 @@
+
+import {types} from "../types";
+
+export const loginSuccess = (user) => ({
+    type: types.LOGIN_SUCCESS,
+    payload: user
+});
+
+
+export const loginFailed = (user) => ({
+    type: types.LOGIN_FAILED  
+});
+
+
+export const loginSuccess = (user) => ({
+    type: types.LOGOUT
+});
